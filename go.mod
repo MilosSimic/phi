4d7c67865dce7573c20a8d1a9f671d8896adf40a
milossimic <milossimicsimo@gmail.com>
2021-07-01 16:59:03 +0200

    basic implementation of phi algorithm

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/MilosSimic/phi
+
+go 1.16
+
+require (
+	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
+	github.com/dgryski/go-onlinestats v0.0.0-20170612111826-1c7d19468768 // indirect
+)
---
+++ phi.go
@@ -0,0 +1,52 @@
+package phi
+
+import (
+	"github.com/dgryski/go-onlinestats"
+	"math"
+	"sync"
+	"time"
+)
+
+type PhiDetector struct {
+	last       time.Time
+	window     *onlinestats.Windowed
+	minSamples int
+	lock       sync.Mutex
+}
+
+func New(windowSize, minSamples int) *PhiDetector {
+	return &PhiDetector{
+		minSamples: minSamples,
+		window:     onlinestats.NewWindowed(windowSize),
+	}
+}
+
+func (p *PhiDetector) Add(t time.Time) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if p.last.IsZero() {
+		p.window.Push(t.Sub(p.last).Seconds())
+	}
+
+	p.last = t
+}
+
+func (p *PhiDetector) Calculate(t time.Time) float64 {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if p.window.Len() < p.minSamples {
+		return 0
+	}
+
+	t1 := t.Sub(p.last).Seconds()
+	before := 1 - distribution(p.window.Mean(), p.window.Stddev(), t1)
+	phi := -math.Log10(before)
+
+	return phi
+}
+
+func distribution(mean, stddev, x float64) float64 {
+	return 0.5 + 0.5*math.Erf((x-mean)/(stddev*math.Sqrt2))
+}
